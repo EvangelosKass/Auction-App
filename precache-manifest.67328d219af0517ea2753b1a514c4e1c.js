48784a65ae532124dc5d04cd63d203c003c24106
EvangelosKass <kassevangelos@gmail.com>
2021-05-19 09:18:59 -0700

    Updates

---
+++ precache-manifest.67328d219af0517ea2753b1a514c4e1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c763f7ebf124b13c630e748e0865020",
+    "url": "/Auction-App/index.html"
+  },
+  {
+    "revision": "f75d34c8bd591adf532a",
+    "url": "/Auction-App/static/css/main.36b87977.chunk.css"
+  },
+  {
+    "revision": "532e532ce1897f5793bf",
+    "url": "/Auction-App/static/js/2.326f8dcc.chunk.js"
+  },
+  {
+    "revision": "f75d34c8bd591adf532a",
+    "url": "/Auction-App/static/js/main.119bcc8c.chunk.js"
+  },
+  {
+    "revision": "be4fc3405bc1b9bd5e6d",
+    "url": "/Auction-App/static/js/runtime-main.3fdaa5ca.js"
+  }
+]);
